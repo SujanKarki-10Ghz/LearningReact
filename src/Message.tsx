3362768f40fee4833b8cbb08d61fc5b24ca73aed
Sujan Karki <83892476+Apocalypse526@users.noreply.github.com>
2023-09-11 14:45:42 +0545

    learning react day 1

---
+++ src/Message.tsx
@@ -0,0 +1,13 @@
+//Pascal Casing
+const Message = () => {
+    //JSX: JavaScript XML
+    return ( 
+        <>
+        <div>
+            <h1>Hello World</h1>
+        </div>
+        </>
+     );
+}
+ 
+export default Message;
